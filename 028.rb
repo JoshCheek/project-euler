e24d12aade5c5b944c40b5d6cd0cb3cd1c669ae8
Josh Cheek <josh.cheek@gmail.com>
2018-02-23 23:33:08 -0600

    Refactor 028, final edition!!

--- 028.rb
+++ 028.rb
@@ -1,9 +1,7 @@
 # https://projecteuler.net/problem=28
 
 def f(n)
-  raise "doesn't make sense to have an even sided spiral!" if n.even?
-  n /= 2
-  (16*n*n*n + 30*n*n + 26*n + 3) / 3
+  (4*n*n*n + 3*n*n + 8*n - 9) / 6
 end
 
 f 5    # => 101
