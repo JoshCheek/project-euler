9f6898af5c0626f2b3beb440630e419d4dc59169
Josh Cheek <josh.cheek@gmail.com>
2018-06-14 22:43:12 -0500

    Update 002 after thinking about it for a blog answer
    https://dev.to/joshcheek/comment/3ne0

--- 002.rb
+++ 002.rb
@@ -1,8 +1,7 @@
 # https://projecteuler.net/problem=2
 a, b, sum = 1, -1, 0
-1.step do |i|
+while a <= 4_000_000
   a, b = a+b, a
-  break if a > 4_000_000
   sum += a if a.even?
 end
 sum # => 4613732
