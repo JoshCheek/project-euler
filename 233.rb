031f5b497f69286de077cd9aae05fe6359a3ff30
Josh Cheek <josh.cheek@gmail.com>
2018-11-25 09:26:52 -0500

    Some hints for myself for 233

---
+++ 233.rb
@@ -0,0 +1,11 @@
+# https://projecteuler.net/problem=233
+# https://www.youtube.com/watch?v=NaL_Cb42WyY&t=31s
+# looks like:
+# find prime conjugates for 420's prime factorization (2*2*3*5*7)
+# then take all the ways of selecting solutions from them
+# then for each of these, multiply by -1, 1, i, -i
+# (note that 2 is a bit unique)
+# ( note that this might be wrong, I haven't completed the video, and also the
+#   circle in the problem is shifted, it's not on the origin, its equation is:
+#   2N**2 = (x-N)**2 + (y-N)**2
+# )
