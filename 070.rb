7a2ae53709159bc6c20856b0b64aae28c171b133
Josh Cheek <josh.cheek@gmail.com>
2018-12-08 20:57:26 +0530

    070 (fixed)

--- 070.rb
+++ 070.rb
@@ -12,7 +12,20 @@ def permutation?(a, b)
   a.digits.sort == b.digits.sort
 end
 
-10_000_000.step by: -1 do |n|
-  break n if permutation? n, totient(n)
+best_totient = 0
+best_number  = Float::INFINITY
+best_ratio   = Float::INFINITY
+10_000_000.downto(2) do |n|
+  p n if n % 500_000 == 0
+  # break if n < best_totient
+  t = totient n
+  next unless permutation? n, t
+  r = n.to_r / t
+  next unless r < best_ratio
+  best_ratio   = r
+  best_number  = n
+  best_totient = t
 end
-# => 9983167
+
+# {:best_number=>8319823, :best_totient=>8313928, :best_ratio=>1.0007090511248113}
+p best_number: best_number, best_totient: best_totient, best_ratio: best_ratio.to_f
