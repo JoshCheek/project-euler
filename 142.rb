01c8245c69d7ba48554055ffd9f4a5a52ab5a372
Josh Cheek <josh.cheek@gmail.com>
2019-02-26 19:51:27 -0600

    142

--- 142.rb
+++ 142.rb
@@ -1,37 +1,36 @@
 require 'set'
-@gen_squares = Enumerator.new do |y|
-  0.step { |i| y << i*i }
-end
-@squares = Set.new
-@max = @gen_squares.next
-@squares << @max
+squares = Set.new
+pairs   = Hash.new { |h, k| h[k] = Set.new }
 
-def square?(n)
-  while @max < n
-    @max = @gen_squares.next
-    @squares << @max
+0.step do |root|
+  square = root * root
+  squares << square
+
+  low   = 1
+  high  = square - 1
+  added = []
+  while low < high
+    # These add up to a square, so the question is: do they subtract to a square, too?
+    # If so, they're a valid pair (x, y) or (x, z) or (y, z)
+    if squares.include? high - low
+      pairs[low]  << high
+      pairs[high] << low
+      added << [low, high]
+    end
+    low += 1
+    high -= 1
   end
-  @squares.include? n
-end
 
-
-gen_pairs = Enumerator.new do |y|
-  2.step do |a|
-    1.upto a.pred do |b|
-      next unless square? a-b
-      next unless square? a+b
-      y << [a, b]
+  # check each of the pairs we just found to see if they each pair with a common third value
+  added.each do |low, high|
+    low_pairs = pairs[low]
+    pairs[high].each do |third|
+      if low_pairs.include? third
+        # took about 14 seconds
+        # {:low=>420968, :high=>434657, :third=>150568, :sum=>1006193}
+        p low: low, high: high, third: third, sum: low + high + third
+        exit
+      end
     end
   end
-end
-
-# keys are always larger than values
-pairs = Hash.new { |h, k| h[k] = [] }
-gen_pairs.each do |x, y|
-  p x: x
-  if (z = (pairs[x] & pairs[y]).min)
-    p x: x, y: y, z: z, sum: x+y+z
-    exit
-  end
-  pairs[x] << y
 end
